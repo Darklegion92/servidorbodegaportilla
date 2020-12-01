bd9ee2defe1f61dda0eca738459f798f361cf948
Desarrollo <onlin1992@gmail.com>
2020-12-01 08:15:58 -0500

    algun error

--- src/services/bancospse.js
+++ src/services/bancospse.js
@@ -1,33 +1,34 @@
-var CronJob = require("cron").CronJob;
-const axios = require("axios");
-const { crearToken } = require("../middlewares/globalPlay");
-const pool = require("../config/database");
+var CronJob = require('cron').CronJob
+const axios = require('axios')
+const { crearToken } = require('../middlewares/globalPlay')
+const pool = require('../config/database')
 
 const bancospse = () =>
   new CronJob(
-    "59 23 * * *",
+    '* * * * *',
     async function () {
-      const AuthToken = await crearToken();
+      const AuthToken = await crearToken()
 
       try {
         const json = await axios.get(
-          "https://noccapi-stg.globalpay.com.co/banks/PSE/",
-          { headers: { "auth-token": AuthToken } }
-        );
+          'https://noccapi-stg.globalpay.com.co/banks/PSE/',
+          { headers: { 'auth-token': AuthToken } }
+        )
+        console.log(json)
+        const bancos = json.data.banks
 
-        const bancos = json.data.banks;
-        await pool.query("DELETE FROM bancospse");
-        bancos.forEach(async (banco) => {
-          const res = await pool.query("INSERT INTO bancospse SET ?", banco);
-        });
+        /*  await pool.query('DELETE FROM bancospse')
+        bancos.forEach(async banco => {
+          await pool.query('INSERT INTO bancospse SET ?', banco)
+        })*/
       } catch (e) {
-        console.log(e);
+        // console.log(e)
       }
     },
     null,
     true
-  );
+  )
 
 module.exports = {
-  bancospse,
-};
+  bancospse
+}
