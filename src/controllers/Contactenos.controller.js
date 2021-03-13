71dbc1e4c07b4905d371a1c528d809fc2edcba5e
Desarrollo <onlin1992@gmail.com>
2021-03-13 08:26:36 -0500

    agregado correo para pruebas

--- src/controllers/Contactenos.controller.js
+++ src/controllers/Contactenos.controller.js
@@ -1,11 +1,53 @@
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer')
 
-function enviarContacto(req, res) {
-  const { nombre, email, telefono, comunicaciones, mensaje } = req.body;
-  console.log(req.body);
+function enviarContacto (req, res) {
+  const { nombre, email, telefono, comunicaciones, mensaje } = req.body
 
-  console.log("Creating transport...");
-  var transporter = nodemailer.createTransport({
+  console.log('Creating transport...')
+
+  let jConfig = {
+    host: 'smtp.gmail.com',
+    port: '465',
+    secure: true,
+    auth: {
+      type: 'login',
+      user: 'contactos.portilla@gmail.com',
+      pass: 'B123456789p.'
+    }
+  }
+
+  var jConfig = {
+    from: 'contactos.portilla@gmail.com',
+    to: 'onlin1992@gmail.com',
+    subject: 'Contacto usuario nuevo',
+    html: (
+      <div>
+        <p>{'Email: ' + email}</p>
+        <p>{'Nombre: ' + nombre}</p>
+        <p>{'Teléfono: ' + telefono}</p>
+        <p>{'Aceptó recibir Comunicaciones:' + comunicaciones}</p>
+        <p>{'Aceptó políticas, Termínos y Condiciones: true'}</p>
+        <p>{'Mensaje: ' + mensaje}</p>
+      </div>
+    )
+  }
+
+  let createTransport = nodemailer.createTransport(jConfig);
+
+  createTransport.sendMail(email, function (error, info) { 
+    if(error){ 
+         console.log("Error al enviar email"); 
+      res.status(501).send({ mensaje: error });
+
+    } else{ 
+         console.log("Correo enviado correctamente"); 
+      res.status(200).send({ mensaje: "Email sent: " + info.response });
+
+    } 
+    createTransport.close(); 
+});
+
+  /*  var transporter = nodemailer.createTransport({
     service: "gmail", //al usar un servicio bien conocido, no es necesario proveer un nombre de servidor.
     auth: {
       user: "contactos.portilla@gmail.com",
@@ -43,17 +85,17 @@ function enviarContacto(req, res) {
       res.status(200).send({ mensaje: "Email sent: " + info.response });
       console.log("Email sent: " + info.response);
     }
-  });
+  });*/
 
-  console.log("End of Script");
+  console.log('End of Script')
 }
 
-function error(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.status(404).send({ mensaje: "Página no encontrada" });
+function error (req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.status(404).send({ mensaje: 'Página no encontrada' })
 }
 
 module.exports = {
   enviarContacto,
-  error,
-};
+  error
+}
